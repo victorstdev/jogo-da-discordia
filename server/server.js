f08ae2a7dd0bde7dbcaea308682c53f793e2fbb3
victorstdev <victorstdev@gmail.com>
2025-12-18 12:50:49 -0300

    mudanças no javascript e inclusão do packages para rodar o webservice no Render

--- server/server.js
+++ server/server.js
@@ -30,4 +30,8 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
